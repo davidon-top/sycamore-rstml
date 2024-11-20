47dc33e3a490a0b663cb98627f3d887023c5c5b3
davidontop <me@davidon.top>
2024-11-20 15:33:37 +0100

    docs: add docs to the html macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod html;
 
 use proc_macro::TokenStream;
 
+/// Alternative to sycamore::view! that uses rstml/html style templating
 #[proc_macro]
 pub fn html(input: TokenStream) -> TokenStream {
     html::Html::new().impl_html(input.into()).into()
